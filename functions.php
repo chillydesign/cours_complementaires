e5da36f8b21215c80119093e66057e104132cf2a
Charles Harvey <harvey.charles@gmail.com>
2019-07-18 12:17:51 +0200

    version 3.6

--- functions.php
+++ functions.php
@@ -174,7 +174,7 @@ function html5blank_styles()
     // wp_register_style('reset', get_template_directory_uri() . '/reset.css', array(), '1.1', 'all');
     // wp_enqueue_style('reset'); // Enqueue it!
 
-    wp_register_style('style', get_template_directory_uri() . '/style.css', array(), '3.5', 'all');
+    wp_register_style('style', get_template_directory_uri() . '/style.css', array(), '3.6', 'all');
     wp_enqueue_style('style'); // Enqueue it!
 }
 
