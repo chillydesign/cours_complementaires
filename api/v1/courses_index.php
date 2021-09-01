142ea2366e971a61b8a1a3a9f23523fa80b16199
Charles Harvey <harvey.charles@gmail.com>
2021-09-01 09:40:15 +0100

    start add multi cats to coursses

--- api/v1/courses_index.php
+++ api/v1/courses_index.php
@@ -1,14 +1,18 @@
 <?php
 
 
-$posts_array = get_posts(array('post_type'  => 'cours', 'posts_per_page' => -1, 'post_status' => 'publish' , 'orderby' => 'post_title', 'order'=> 'ASC' ) );
-$post_ids =  array_map( function($p){ return $p->ID; } , $posts_array);
+$posts_array = get_posts(array('post_type'  => 'cours', 'posts_per_page' => -1, 'post_status' => 'publish', 'orderby' => 'post_title', 'order' => 'ASC'));
+$post_ids =  array_map(function ($p) {
+    return $p->ID;
+}, $posts_array);
 $course_categories = api_get_course_cats($post_ids);
 $course_schools = api_get_course_schools($post_ids);
 $course_descriptions = api_get_course_descriptions($post_ids);
 $course_extras = api_get_course_extras($post_ids);
 $course_locations = api_get_course_locations($post_ids);
-$location_ids =  array_map(function($p) {  return $p->wid; } , $course_locations);
+$location_ids =  array_map(function ($p) {
+    return $p->wid;
+}, $course_locations);
 $course_zones = api_get_course_zones($location_ids);
 $course_professeurs = api_get_course_professeurs($post_ids);
 // $course_ecolages = api_get_course_ecolages($post_ids);
@@ -32,7 +36,16 @@ foreach ($posts_array as $post) {
     );
     $post->categories = array_values($cats);
 
-
+    if (sizeof($post->categories) > 0) {
+        $post->cat_names = implode(
+            ',',
+            array_map(function ($c) {
+                return $c->name;
+            }, $post->categories)
+        );
+    } else {
+        $post->cat_names = false;
+    }
 
 
 
@@ -54,9 +67,9 @@ foreach ($posts_array as $post) {
     }
 
 
-   
 
-    if ( strlen($post->school) < 3   ) {
+
+    if (strlen($post->school) < 3) {
         $post->school = [$post->school];
     } else {
         $uns_sch = unserialize($post->school);
@@ -70,7 +83,9 @@ foreach ($posts_array as $post) {
             return $e->wppid == $post->ID;
         }
     );
-    $locations =  array_values(array_map(function($p){ return $p->wid; }, $location));
+    $locations =  array_values(array_map(function ($p) {
+        return $p->wid;
+    }, $location));
     $post->location = $locations;
 
 
@@ -82,9 +97,10 @@ foreach ($posts_array as $post) {
                 return  in_array($e->wppid, $post->location);
             }
         );
-        $zones =  array_values( array_map( function($l) {return $l->wid;}, $zone));
+        $zones =  array_values(array_map(function ($l) {
+            return $l->wid;
+        }, $zone));
         $post->zone = $zones;
-    
     } else {
         $post->zone =  [];
     }
@@ -101,25 +117,21 @@ foreach ($posts_array as $post) {
     );
     $prof_array = array();
     foreach ($professuers as $post_meta) {
-        if ($post_meta->meta_key[0] != '_' && $post_meta->meta_value != '' ) {
+        if ($post_meta->meta_key[0] != '_' && $post_meta->meta_value != '') {
 
-            if (  strlen($post_meta->meta_value) > 1 ) {
-                if ( $post_meta->meta_value[1] == ':' ) {  // it is a serialised array
+            if (strlen($post_meta->meta_value) > 1) {
+                if ($post_meta->meta_value[1] == ':') {  // it is a serialised array
                     $unserialised = unserialize($post_meta->meta_value);
 
                     foreach ($unserialised as $teacher_id) {
-                        array_push($prof_array,  $teacher_id );
+                        array_push($prof_array,  $teacher_id);
                     }
-
                 } else {
                     //    array_push($prof_array,  $post_meta->meta_value );
                 }
-
             } else {
-                array_push($prof_array,  $post_meta->meta_value );
+                array_push($prof_array,  $post_meta->meta_value);
             }
-
-
         }
     }
     $post->professuers = array_values(array_unique($prof_array));
@@ -130,8 +142,8 @@ foreach ($posts_array as $post) {
 
 
 
-    if (sizeof( $post->categories) > 0) {
-        $post->code = $school_letter[$post->school[0] ] . $category_letters[ $post->categories[0]->term_id  ]  .   $post->ID;
+    if (sizeof($post->categories) > 0) {
+        $post->code = $school_letter[$post->school[0]] . $category_letters[$post->categories[0]->term_id]  .   $post->ID;
     } else {
         $post->code = '-';
     }
@@ -152,7 +164,7 @@ foreach ($posts_array as $post) {
 
     if ($des) {
         $des = $des->meta_value;
-        $post->description = ($des != null) ? implode(' ', array_slice(explode(' ', strip_tags($des)),0,20)  ) . '...'  : '';
+        $post->description = ($des != null) ? implode(' ', array_slice(explode(' ', strip_tags($des)), 0, 20)) . '...'  : '';
         #$post->description = ($des != null) ? $des : ''; 
     } else {
         $post->description = '';
@@ -176,13 +188,13 @@ foreach ($posts_array as $post) {
 
 
 
-    $searchable_fields = array($post->post_title,$post->description,$post->extra,$post->code);
+    $searchable_fields = array($post->post_title, $post->description, $post->extra, $post->code);
     $searchable_fields = implode('  ', $searchable_fields);
     $searchable_fields = wp_strip_all_tags($searchable_fields);
 
 
-    $searchable_fields = str_replace(array("\n","\r"), ' ', $searchable_fields);
-    $searchable_fields = str_replace(array("!","’" , ',' , '/' ,':', "?", '.', '–' ), ' ', $searchable_fields);
+    $searchable_fields = str_replace(array("\n", "\r"), ' ', $searchable_fields);
+    $searchable_fields = str_replace(array("!", "’", ',', '/', ':', "?", '.', '–'), ' ', $searchable_fields);
     //$searchable_fields = preg_replace( '/[^A-Za-z0-9_-]/', '', $searchable_fields );
     $searchable_fields = remove_accents($searchable_fields);
     $searchable_fields = strtolower($searchable_fields);
@@ -217,20 +229,16 @@ foreach ($posts_array as $post) {
 
 
     // remove unncessary params
-    $unncessary_params = ['comment_count', 'post_status', 'post_mime_type',  'ping_status', 'comment_status', 'menu_order', 'post_parent' , 'post_date_gmt',  'post_modified_gmt', 'post_password',  'post_excerpt', 'pinged', 'to_ping', 'filter', 'post_content_filtered'];
+    $unncessary_params = ['comment_count', 'post_status', 'post_mime_type',  'ping_status', 'comment_status', 'menu_order', 'post_parent', 'post_date_gmt',  'post_modified_gmt', 'post_password',  'post_excerpt', 'pinged', 'to_ping', 'filter', 'post_content_filtered'];
     foreach ($unncessary_params as $up) {
         unset($post->$up);
     }
-
-
-
-
 }
 
 
 
 
-echo json_encode( $posts_array ,  JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT|JSON_NUMERIC_CHECK );
+echo json_encode($posts_array,  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK);
 
 
 
@@ -243,6 +251,3 @@ echo json_encode( $posts_array ,  JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|
 // );
 // $loc =  reset(array_values($location))->wid ;
 // $post->location = ($loc != null) ? $loc : '';
-
-
-?>
