1303c5d71370541a9cabe9999035e04c2f7451ae
Charles Harvey <harvey.charles@gmail.com>
2023-03-06 23:21:05 +0000

    change cmg address

--- sections/schools.php
+++ sections/schools.php
@@ -1,4 +1,4 @@
-<?php $search_url =  get_permalink( get_page_by_path( 'recherche' ) ); ?>
+<?php $search_url =  get_permalink(get_page_by_path('recherche')); ?>
 
 
 <div class="container">
@@ -6,7 +6,7 @@
 		<div class="col-sm-4">
 			<div class="school_logo ">
 
-				<div class="flip-container"  ontouchstart="this.classList.toggle('hover');" >
+				<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
 
 					<div class="flipper">
 						<div class="front sc_lo_cg">
@@ -16,10 +16,18 @@
 							<h3>CMG</h3>
 							<h4>Conservatoire de musique de Genève</h4>
 							<p>
-								<div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div><div class="next_to_icon">+41 22 319 60 60</div><div class="clear"></div>
-								<div class="icon"><i class="fa fa-envelope" aria-hidden="true"></i></div><div class="next_to_icon"> cmg@cmg.ch</div><div class="clear"></div>
-								<div class="icon"><i class="fa fa-mouse-pointer" aria-hidden="true"></i></div><div class="next_to_icon"> <a href="http://www.cmg.ch/">www.cmg.ch/</a></div><div class="clear"></div>
-								<div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div><div class="next_to_icon"> Bd du théâtre 5<br/>Case postale 5155<br/>1211 Genève 11</div><div class="clear"></div>
+							<div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
+							<div class="next_to_icon">+41 22 319 60 60</div>
+							<div class="clear"></div>
+							<div class="icon"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+							<div class="next_to_icon"> cmg@cmg.ch</div>
+							<div class="clear"></div>
+							<div class="icon"><i class="fa fa-mouse-pointer" aria-hidden="true"></i></div>
+							<div class="next_to_icon"> <a href="http://www.cmg.ch/">www.cmg.ch/</a></div>
+							<div class="clear"></div>
+							<div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+							<div class="next_to_icon">Bd du Théâtre 5 <br> 1204 Genève</div>
+							<div class="clear"></div>
 							</p>
 						</div>
 					</div>
@@ -32,19 +40,25 @@
 		<div class="col-sm-4">
 			<div class="school_logo ">
 
-				<div class="flip-container"  ontouchstart="this.classList.toggle('hover');">
+				<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
 
 					<div class="flipper">
 						<div class="front sc_lo_cp">
-					<!-- 		<a href="<?php echo $search_url . '#e=4'; ?>">Conservatoire populaire musique danse théâtre</a> -->
+							<!-- 		<a href="<?php echo $search_url . '#e=4'; ?>">Conservatoire populaire musique danse théâtre</a> -->
 						</div>
 						<div class="back sc_lo_cp">
 							<h3>CPMDT</h3>
 							<h4>Conservatoire populaire de musique, danse et théâtre</h4>
 							<p>
-								<div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div><div class="next_to_icon">+41 22 329 67 22</div><div class="clear"></div>
-								<div class="icon"><i class="fa fa-mouse-pointer" aria-hidden="true"></i></div><div class="next_to_icon"> <a href="http://www.conservatoirepopulaire.ch/"><span style="display:inline-block">www.conservatoire</span><span style="display:inline-block">populaire.ch</span></a></div><div class="clear"></div>
-								<div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div><div class="next_to_icon">Rue François-D'Ivernois 7<br>Case postale 230<br>1211 Genève 12</div><div class="clear"></div>
+							<div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
+							<div class="next_to_icon">+41 22 329 67 22</div>
+							<div class="clear"></div>
+							<div class="icon"><i class="fa fa-mouse-pointer" aria-hidden="true"></i></div>
+							<div class="next_to_icon"> <a href="http://www.conservatoirepopulaire.ch/"><span style="display:inline-block">www.conservatoire</span><span style="display:inline-block">populaire.ch</span></a></div>
+							<div class="clear"></div>
+							<div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+							<div class="next_to_icon">Rue François-D'Ivernois 7<br>Case postale 230<br>1211 Genève 12</div>
+							<div class="clear"></div>
 							</p>
 						</div>
 					</div>
@@ -58,20 +72,28 @@
 		<div class="col-sm-4">
 			<div class="school_logo ">
 
-				<div class="flip-container" ontouchstart="this.classList.toggle('hover');" >
+				<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
 
 					<div class="flipper">
 						<div class="front sc_lo_ijd">
-	<!-- 						<a href="<?php echo $search_url . '#e=38'; ?>">institut Jaques-dalcroze</a> -->
+							<!-- 						<a href="<?php echo $search_url . '#e=38'; ?>">institut Jaques-dalcroze</a> -->
 						</div>
 						<div class="back sc_lo_ijd">
 							<h3>IJD</h3>
 							<h4>Institut Jaques-dalcroze</h4>
 							<p>
-								<div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div><div class="next_to_icon">+41 22 718 37 60</div><div class="clear"></div>
-								<div class="icon"><i class="fa fa-envelope" aria-hidden="true"></i></div><div class="next_to_icon">info@dalcroze.ch</div><div class="clear"></div>
-								<div class="icon"><i class="fa fa-mouse-pointer" aria-hidden="true"></i></div><div class="next_to_icon"><a href="http://www.dalcroze.ch/">www.dalcroze.ch/</a></div><div class="clear"></div>
-								<div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div><div class="next_to_icon">Rue de la Terrassière 44<br>1207 Genève</div><div class="clear"></div>
+							<div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
+							<div class="next_to_icon">+41 22 718 37 60</div>
+							<div class="clear"></div>
+							<div class="icon"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+							<div class="next_to_icon">info@dalcroze.ch</div>
+							<div class="clear"></div>
+							<div class="icon"><i class="fa fa-mouse-pointer" aria-hidden="true"></i></div>
+							<div class="next_to_icon"><a href="http://www.dalcroze.ch/">www.dalcroze.ch/</a></div>
+							<div class="clear"></div>
+							<div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+							<div class="next_to_icon">Rue de la Terrassière 44<br>1207 Genève</div>
+							<div class="clear"></div>
 							</p>
 						</div>
 					</div>
